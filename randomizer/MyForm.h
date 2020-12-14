86e3aed7ab63f18ec55c117bbf1e4e0b58cea569
T3iNE <T3iNe@users.noreply.github.com>
2020-12-14 15:20:13 +0500

    first upload

---
+++ randomizer/MyForm.h
@@ -0,0 +1,249 @@
+#pragma once
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+#include <fstream>
+
+namespace randomizer {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace std;
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	protected:
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Button^ button1;
+
+
+
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container^ components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(51, 302);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(468, 21);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Очистка";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(88, 18);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(166, 13);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Введите размерность массива";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(149, 52);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(145, 20);
+			this->textBox1->TabIndex = 3;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// textBox2
+			// 
+			this->textBox2->AcceptsReturn = true;
+			this->textBox2->AcceptsTab = true;
+			this->textBox2->Location = System::Drawing::Point(51, 138);
+			this->textBox2->Multiline = true;
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->ScrollBars = System::Windows::Forms::ScrollBars::Both;
+			this->textBox2->Size = System::Drawing::Size(468, 158);
+			this->textBox2->TabIndex = 4;
+			this->textBox2->WordWrap = false;
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox2_TextChanged);
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(313, 52);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(206, 60);
+			this->button4->TabIndex = 6;
+			this->button4->Text = L"Сгененировать";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(149, 92);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(145, 20);
+			this->textBox3->TabIndex = 7;
+			this->textBox3->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox3_TextChanged);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(48, 52);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(0, 13);
+			this->label2->TabIndex = 8;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(48, 95);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(73, 13);
+			this->label3->TabIndex = 9;
+			this->label3->Text = L"Кол-во строк";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(48, 55);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(91, 13);
+			this->label4->TabIndex = 10;
+			this->label4->Text = L"Кол-во столбцов";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(633, 149);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 11;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(985, 335);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button2);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+		int M = 0;
+		int N = 0;
+		int** arra = new int* [M];
+		private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+
+	}
+private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+	M = System::Convert::ToInt32(textBox3->Text);
+	N = System::Convert::ToInt32(textBox1->Text);
+	srand(time(NULL));
+	for (int i = 0; i < M; i++) {
+		arra[i] = new int[N];
+	}
+	ofstream file("1.txt");
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			arra[i][j] = rand();
+			textBox2->Text += System::Convert::ToString(arra[i][j]+"\t");
+			file << arra[i][j] << "\t";
+		}
+		file << "\n";
+		textBox2->Text += System::Convert::ToString("\r\n");
+	}
+	file.close();
+}
+private: System::Void textBox3_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (arra) {
+		for (int i = 0; i < M; ++i) {
+			delete[] arra[i];
+		}
+		delete[] arra;
+		textBox2->Text = " ";
+	}
+	fstream clear_file("1.txt", ios::out);
+	clear_file.close();
+}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	textBox2->Text += arra[1][1];
+}
+};
+}
